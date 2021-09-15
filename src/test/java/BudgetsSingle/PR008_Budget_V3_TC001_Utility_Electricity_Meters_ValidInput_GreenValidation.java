7e7e5ed9e25e03bc19fcfc6bfe426c6bbb2780d7
Swapnja <swapnja.gudle@cloudenergysoftware.com>
2021-09-15 19:24:19 +0530

    BudgetSingle folder created for sample

---
+++ src/test/java/BudgetsSingle/PR008_Budget_V3_TC001_Utility_Electricity_Meters_ValidInput_GreenValidation.java
@@ -0,0 +1,31 @@
+//Meters_Green 		Validation_Green  	 Status:Passed
+
+package BudgetsSingle;
+
+import com.Budgets.AbstractTestNg;
+import com.Budgets.PageHandlers.CommonPageHandler;
+import com.Budgets.PageHandlers.CustomersPageHandler;
+import com.Budgets.PageHandlers.ImportPageHandlerElectricity;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class PR008_Budget_V3_TC001_Utility_Electricity_Meters_ValidInput_GreenValidation extends AbstractTestNg {
+	public PR008_Budget_V3_TC001_Utility_Electricity_Meters_ValidInput_GreenValidation() throws IOException {
+	}
+
+	@Test
+	public void newBudget() throws InterruptedException, IOException {
+		CustomersPageHandler customersPageHandler = new CustomersPageHandler(webDriverHandler, prop);
+		customersPageHandler.selectFirstCustomer();
+		CommonPageHandler budgetPageHandler = new CommonPageHandler(webDriverHandler, prop);
+		// budgetPageHandler.waitLoad();
+		budgetPageHandler.createBudgetMonthlyElectricity();
+		ImportPageHandlerElectricity importPageHandler = new ImportPageHandlerElectricity(webDriverHandler, prop);
+		importPageHandler.ImportMeterGreenFile();
+		WebElement completedImage = importPageHandler.getGreenIcon("MeterLocations");
+		System.out.println("Meter Successful Marked: " + completedImage.isDisplayed());
+		importPageHandler.verifyImport();
+	}
+}
