7148cbf33063c0bee5c672742c544079e5180590
Swapnja <swapnja.gudle@cloudenergysoftware.com>
2021-10-04 12:34:00 +0530

    commiting the changes inElectricity 4 as per class name changing method name

---
+++ src/test/java/com/Budgets/Utility/Electricity/Electricity4/PR008_Budget_V3_TC086_Utility_Electricity_PassthroughCharges_PublishedView_ValidInput_AmberValidation_CancelledImport.java
@@ -0,0 +1,36 @@
+//Budget_Passthough_Published View			Validation_Amber  Status:Passed
+package com.Budgets.Utility.Electricity.Electricity4;
+
+import com.Budgets.AbstractTestNg;
+import com.Budgets.PageHandlers.CommonPageHandler;
+import com.Budgets.PageHandlers.CustomersPageHandler;
+import com.Budgets.PageHandlers.ImportPageCancelledStatus;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class PR008_Budget_V3_TC086_Utility_Electricity_PassthroughCharges_PublishedView_ValidInput_AmberValidation_CancelledImport
+		extends AbstractTestNg {
+
+	public PR008_Budget_V3_TC086_Utility_Electricity_PassthroughCharges_PublishedView_ValidInput_AmberValidation_CancelledImport()
+			throws IOException {
+
+	}
+
+	@Test
+	public void BudgetConsoleVerifyTest() throws InterruptedException, IOException {
+		CustomersPageHandler customersPageHandler = new CustomersPageHandler(webDriverHandler, prop);
+		customersPageHandler.waitLoad();
+		customersPageHandler.filterCustomers("Automation");
+		customersPageHandler.selectFirstCustomer();
+		CommonPageHandler budgetPageHandler = new CommonPageHandler(webDriverHandler, prop);
+		budgetPageHandler.waitLoad();
+		budgetPageHandler.selectFirstBudgetElectricity();
+		ImportPageCancelledStatus importPageCancelledStatusElectricity = new ImportPageCancelledStatus(webDriverHandler,
+				prop);
+		importPageCancelledStatusElectricity.ImportFileCancel("ElectricityPublishedView",
+				"BudgetTestData/Electricity/PublishedView/PR008_Budget_V3_TC096_Utility_Electricity_PassthroughCharges_PublishedView_ValidInput_AmberValidation_CancelledImport.xlsx");
+		importPageCancelledStatusElectricity.verifyImport();
+	}
+
+}
---
+++ src/test/java/com/Budgets/Utility/Electricity/Electricity4/PR008_Budget_V3_TC087_Utility_Electricity_PassthroughCharges_InspiredEnergyView_ValidInput_GreenValidation_CancelledImport.java
@@ -0,0 +1,37 @@
+//Budget_PassthroughCharges_InspiredView	Validatin:Green  Status:Passed
+
+package com.Budgets.Utility.Electricity.Electricity4;
+
+import com.Budgets.AbstractTestNg;
+import com.Budgets.PageHandlers.CommonPageHandler;
+import com.Budgets.PageHandlers.CustomersPageHandler;
+import com.Budgets.PageHandlers.ImportPageCancelledStatus;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class PR008_Budget_V3_TC087_Utility_Electricity_PassthroughCharges_InspiredEnergyView_ValidInput_GreenValidation_CancelledImport
+		extends AbstractTestNg {
+
+	public PR008_Budget_V3_TC087_Utility_Electricity_PassthroughCharges_InspiredEnergyView_ValidInput_GreenValidation_CancelledImport()
+			throws IOException {
+
+	}
+
+	@Test
+	public void BudgetConsoleVerifyTest() throws InterruptedException, IOException {
+		CustomersPageHandler customersPageHandler = new CustomersPageHandler(webDriverHandler, prop);
+		customersPageHandler.waitLoad();
+		customersPageHandler.filterCustomers("Automation");
+		customersPageHandler.selectFirstCustomer();
+		CommonPageHandler budgetPageHandler = new CommonPageHandler(webDriverHandler, prop);
+		budgetPageHandler.waitLoad();
+		budgetPageHandler.selectFirstBudgetElectricity();
+		ImportPageCancelledStatus importPageCancelledStatusElectricity = new ImportPageCancelledStatus(webDriverHandler,
+				prop);
+		importPageCancelledStatusElectricity.ImportFileCancel("ElectricityInspiredEnergyView",
+				"BudgetTestData/Electricity/InspiredEnergyView/PR008_Budget_V3_TC041_Utility_Electricity_PassthroughCharges_InspiredEnergyView_ValidInput_GreenValidation.xlsx");
+		importPageCancelledStatusElectricity.verifyImport();
+	}
+
+}
