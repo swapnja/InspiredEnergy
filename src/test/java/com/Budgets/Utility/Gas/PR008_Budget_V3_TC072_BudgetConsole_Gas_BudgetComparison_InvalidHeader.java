d0e99a73367bcdd3b88363a6905b9e14efa3959b
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-02-11 13:12:42 +0000

    xpath has changed

--- src/test/java/com/Budgets/Utility/Gas/PR008_Budget_V3_TC072_BudgetConsole_Gas_BudgetComparison_InvalidHeader.java
+++ src/test/java/com/Budgets/Utility/Gas/PR008_Budget_V3_TC072_BudgetConsole_Gas_BudgetComparison_InvalidHeader.java
@@ -2,8 +2,11 @@
 package com.Budgets.Utility.Gas;
 
 import com.Base.FIleImportUpload;
+import com.Base.SeleniumHandlers.ElementHandler;
 import com.Budgets.AbstractTestNg;
 import com.Budgets.PageHandlers.CustomersPageHandler;
+
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -32,13 +35,17 @@ public class PR008_Budget_V3_TC072_BudgetConsole_Gas_BudgetComparison_InvalidHea
         customersPageHandler.waitLoad();
         FIleImportUpload fim = new FIleImportUpload(driver, xlPathMeter);
         
-        System.out.println("Error message for Invalid header validation is:");
-        //String strErrortext = webDriverHandler.byXpath("ErrorMsg").waitClickable().waitVisible().getText();
-        String strErrortext = webDriverHandler.byId("swal2-content").getText();
-       // String strErrortext = webDriverHandler.byXpath(prop.getProperty("ErrorMsg")).waitClickable().getText();
-        System.out.println(strErrortext);
-        Assert.assertEquals(strErrortext, "Upload template is not valid");
-        webDriverHandler.byXpath("//button[contains(text(),'OK')]").waitClickable().click();
-        System.out.println(strErrortext);
+        System.out.println("*************************************");
+		System.out.println("Error message for Invalid header validation is:");
+		/*
+		 * ElementHandler w1=webDriverHandler.
+		 * byXpath("//div[contains(text(),'Upload template is not valid')]");
+		 * w1.isDisplayed(); String text = w1.getText(); System.out.println(text);
+		 * Assert.assertEquals(text, "Upload template is not valid");
+		 */
+		String strErrortext = webDriverHandler.byXpath("//div[contains(text(),'Upload template is not valid')]").waitClickable().waitVisible().getText();
+		Assert.assertEquals(strErrortext, "Upload template is not valid");
+		webDriverHandler.byXpath("//button[contains(text(),'OK')]").waitClickable().click();
+        //System.out.println(strErrortext);
     }
 }
