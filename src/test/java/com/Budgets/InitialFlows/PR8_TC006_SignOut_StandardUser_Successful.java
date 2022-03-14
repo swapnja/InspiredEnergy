a2cb82d9a46973487c8cfe435bc079a19540a4f2
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-03-14 12:53:20 +0000

    Budget-tc06

--- src/test/java/com/Budgets/InitialFlows/PR8_TC006_SignOut_StandardUser_Successful.java
+++ src/test/java/com/Budgets/InitialFlows/PR8_TC006_SignOut_StandardUser_Successful.java
@@ -43,11 +43,11 @@ public class PR8_TC006_SignOut_StandardUser_Successful {
 		driver.findElement(By.xpath(prop.getProperty("SignOut"))).click();
 		String act_SignOuttitle = driver.getTitle();
 		String exp_SignOuttitle = "Login - Unify";
-		if (act_SignOuttitle.equalsIgnoreCase(exp_SignOuttitle)) {
-			System.out.println("Sign Out functionality is working fine for Standard User");
-		} else {
-			System.out.println("Oops, There is some Issue");
-		}
+		/*
+		 * if (act_SignOuttitle.equalsIgnoreCase(exp_SignOuttitle)) {
+		 * System.out.println("Sign Out functionality is working fine for Standard User"
+		 * ); } else { System.out.println("Oops, There is some Issue"); }
+		 */
 		//driver.quit();
 	}
 
