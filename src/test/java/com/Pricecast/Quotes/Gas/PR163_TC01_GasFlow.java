511c600b0819b332b7e572a2d71572bf31cff234
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-04-26 06:59:48 +0000

    All Gas flow changes are done and scripts are working fine as expected

--- src/test/java/com/Pricecast/Quotes/Gas/PR163_TC01_GasFlow.java
+++ src/test/java/com/Pricecast/Quotes/Gas/PR163_TC01_GasFlow.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class PR163_TC01_GasFlow extends AbstractTestNg {
     public PR163_TC01_GasFlow() throws IOException {
     }
-
+//done
     @Test
     public void AddNewGasQuote_Success() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -19,21 +19,21 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.excelFileDownload();
         gasQuote.pdfFileDownload();
     }
-
+//done
     @Test
     public void AddNewGasQuote_Validation() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
         gasQuote.addNewGasQuoteValidation("AAAQ1 2WE", 22222200, "British Gas");
         gasQuote.gasQuote_Validation();
     }
-
+//done
     @Test
     public void AddNewGasQuote_MandatoryValidation() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
         gasQuote.addNewGasQuoteMandatoryValidation("British Gas");
         gasQuote.gasQuote_MandatoryValidation();
     }
-
+//done
     @Test
     public void AddNewGasQuote_AddUserDefinedQuotesCurrent() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -41,7 +41,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.quoteSelectionUserDefinedQuotes();
         gasQuote.addUserDefinedQuote("Current", 5, 2, 1);
     }
-
+//done
     @Test
     public void AddNewGasQuote_AddUserDefinedQuotesRenewal() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -49,7 +49,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.quoteSelectionUserDefinedQuotes();
         gasQuote.addUserDefinedQuote("Renewal", 5, 2, 1);
     }
-
+//done
     @Test
     public void AddNewGasQuote_AddUserDefinedQuotesDeemed() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -57,15 +57,15 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.quoteSelectionUserDefinedQuotes();
         gasQuote.addUserDefinedQuote("Deemed", 5, 2, 1);
     }
-
+//done
     @Test
     public void AddNewGasQuote_AddUserDefinedQuotesBrokerBespoke() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
         gasQuote.addNewGasQuote("AQ1 2WE", 20000, "British Gas");
         gasQuote.quoteSelectionUserDefinedQuotes();
-        gasQuote.addUserDefinedQuoteBespoke("Broker Bespoke", "Avanti Gas", 5, 1, 1, 2);
+        gasQuote.addUserDefinedQuoteBespoke("Broker Bespoke", "British Gas", 5, 1, 1, 2);
     }
-
+//done
     @Test
     public void AddNewGasQuote_AddUserDefinedQuotesCustomerOther() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -73,7 +73,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.quoteSelectionUserDefinedQuotes();
         gasQuote.addUserDefinedQuoteCustomer("Customer Other", "British Gas", 5, 1, 1);
     }
-
+//done
     @Test
     public void AddNewGasQuote_QuoteComparisonCurrent() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -82,7 +82,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.addUserDefinedQuote("Current", 5, 2, 1);
         gasQuote.quoteComparison();
     }
-
+//done
     @Test
     public void AddNewGasQuote_QuoteComparisonRenewal() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -91,7 +91,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.addUserDefinedQuote("Renewal", 5, 2, 1);
         gasQuote.quoteComparison();
     }
-
+//done
     @Test
     public void AddNewGasQuote_QuoteComparisonDeemed() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
@@ -100,7 +100,7 @@ public class PR163_TC01_GasFlow extends AbstractTestNg {
         gasQuote.addUserDefinedQuote("Deemed", 5, 2, 1);
         gasQuote.quoteComparison();
     }
-
+//done
     @Test
     public void AddNewGasQuote_QuoteComparisonCustomerOther() {
         GasQuote gasQuote = new GasQuote(webDriverHandler);
