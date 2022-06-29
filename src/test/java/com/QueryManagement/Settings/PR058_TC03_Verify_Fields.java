7f025e834e16be0d76772f559fd7e00a1fde8272
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-06-29 09:42:58 +0000

    QMP-Verify Fields-Test is done

--- src/test/java/com/QueryManagement/Settings/PR058_TC03_Verify_Fields.java
+++ src/test/java/com/QueryManagement/Settings/PR058_TC03_Verify_Fields.java
@@ -10,29 +10,31 @@ public class PR058_TC03_Verify_Fields extends AbstractTestNg {
 
     public PR058_TC03_Verify_Fields() throws IOException{
     }
-
+//Pass
     @Test
     public void AccessFields() {
         Fields fields = new Fields(webDriverHandler);
-        fields.accessSettings();
+      //  fields.accessSettings();
         fields.accessFields();
         fields.columnVerification();
-        fields.createFields("Automation Field", "Text", "The description that is required for this TB", true, false);
-        Assert.fail();
+        //fields.createFields("Automation Field", "Text", "The description that is required for to test thh Field functionality",true, false);
+        //marking status false to true to make teh status as Active
+        fields.createFields("Automation Field", "Text", "The description that is required for to test the Field functionality","Automation Tool-tip", true);
+        //Assert.fail();
     }
-
+//Pass
     @Test
     public void EditFields() {
         Fields fields = new Fields(webDriverHandler);
-        fields.accessSettings();
+       // fields.accessSettings();
         fields.accessFields();
-        fields.editFields("Test Field", "Testdfsdf", "This is the new description for the field we created.", false, false);
+        fields.editFields("Automation Field", "Automation Field Edited", "This is the new description for the field we created.", "Automation Tool-tip Edit", true);
     }
-
+//Pass
     @Test
     public void Validations() {
         Fields fields = new Fields(webDriverHandler);
-        fields.accessSettings();
+       // fields.accessSettings();
         fields.accessFields();
         fields.errorValidation();
     }
