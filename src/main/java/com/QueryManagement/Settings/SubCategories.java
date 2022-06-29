08685c20efc6cbfab07a2f43836a1196c7afed4d
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-06-29 08:18:39 +0000

    QMp-commonMethod-SubCategory modified

--- src/main/java/com/QueryManagement/Settings/SubCategories.java
+++ src/main/java/com/QueryManagement/Settings/SubCategories.java
@@ -27,6 +27,7 @@ public class SubCategories {
     ElementHandler tglGlobal;
     ElementHandler tglGlobalCheck;
     ElementHandler ddDefaultAssignment;
+    ElementHandler textAreadefaultSummary;
     ElementHandler ddNextStep;
     ElementHandler tbDueDate;
     ElementHandler tglDueWorkDays;
@@ -56,12 +57,15 @@ public class SubCategories {
     ElementHandler msgActionDate;
     ElementHandler msgToClose;
     ElementHandler btnOk;
+    ElementHandler btnSuccess;
 
     public SubCategories(WebDriverHandler webDriverHandler) {
         _webDriverHandler = webDriverHandler;
         btnSettings = _webDriverHandler.byXpath("//*[@id='btnSetting']");
         tabSubCategories = _webDriverHandler.byXpath("//a[contains(text(), 'Sub-Categories')]");
-        btnNewSubCategory = _webDriverHandler.byXpath("//button[text() = ' Sub-Categories']");
+        //       btnNewSubCategory = _webDriverHandler.byXpath("//button[text() = 'Sub-Categories']");
+        btnNewSubCategory = _webDriverHandler.byXpath("//*[@id='partialViewContent']/div[1]/button[contains(text(),' Sub-Category')]");
+        
         tbSearch = _webDriverHandler.byXpath("//input[@type='search']");
         tbSubCategoryName = _webDriverHandler.byXpath("//*[@id='Name']");
         ddCategory = _webDriverHandler.byXpath("//*[@id='CategoryId']");
@@ -75,9 +79,14 @@ public class SubCategories {
         tglSupplierAccessCheck = _webDriverHandler.byXpath("//label[input[@name='SupplierAccess']]//input");
         tglLicenseeAccess = _webDriverHandler.byXpath("//label[input[@name='LicenseeAccess']]");
         tglLicenseeAccessCheck = _webDriverHandler.byXpath("//label[input[@name='LicenseeAccess']]//input");
-        tglGlobal = _webDriverHandler.byXpath("//label[input[@name='Global']]");
-        tglGlobalCheck = _webDriverHandler.byXpath("//label[input[@name='Global']]//input");
+		/*
+		 * tglGlobal = _webDriverHandler.byXpath("//label[input[@name='Global']]");
+		 * tglGlobalCheck =
+		 * _webDriverHandler.byXpath("//label[input[@name='Global']]//input");
+		 */
         ddDefaultAssignment = _webDriverHandler.byXpath("//*[@id='AccManagerRoleId']");
+        textAreadefaultSummary = _webDriverHandler.byXpath("//*[@id='DefaultSummary']");
+        
         ddNextStep = _webDriverHandler.byXpath("//*[@id='NextStep']");
         tbDueDate = _webDriverHandler.byXpath("//*[@id='DueDateInterval']");
         tglDueWorkDays = _webDriverHandler.byXpath("//label[input[@name='DueInWorkDays']]");
@@ -86,7 +95,8 @@ public class SubCategories {
         tbActionDate = _webDriverHandler.byXpath("//*[@id='NextActionInterval']");
         tglActionWorkDays = _webDriverHandler.byXpath("//label[input[@name='ActionIntervalInWorkDays']]");
         tglActionWorkDaysCheck = _webDriverHandler.byXpath("//label[input[@name='ActionIntervalInWorkDays']]//input");
-        ddToClose = _webDriverHandler.byXpath("//button[contains(@class, 'multiselect dropdown-toggle')]");
+       // ddToClose = _webDriverHandler.byXpath("//button[contains(@class, 'multiselect dropdown-toggle')]");
+        ddToClose = _webDriverHandler.byXpath(" //div[contains(@class,'btn-group show')]//button[contains(@class, 'multiselect dropdown-toggle')]");
         tglStatus = _webDriverHandler.byXpath("//label[input[@name='IsActive']]");
         tglStatusCheck = _webDriverHandler.byXpath("//label[input[@name='IsActive']]//input");
         tglCopyFields = _webDriverHandler.byXpath("//label[input[@name='CopyFields']]");
@@ -95,7 +105,7 @@ public class SubCategories {
         tglCopyActionsCheck = _webDriverHandler.byXpath("//label[input[@name='CopyActions']]//input");
         btnEdit = _webDriverHandler.byXpath("//button[@title='Action']//i");
         btnCopy = _webDriverHandler.byXpath("//button[@title='Copy']//i");
-        btnCreate = _webDriverHandler.byXpath("//form[@id = 'modalAddNewSubCategory']//*[contains(text(), ' Create')]");
+        btnCreate = _webDriverHandler.byXpath("//*[@id='modalAddNewSubCategory']//button[@type='submit']");
         btnCancel = _webDriverHandler.byXpath("//form[@id = 'modalAddNewSubCategory']//*[contains(text(), 'Cancel')]");
         btnSave = _webDriverHandler.byXpath("//form[@id = 'modalAddNewSubCategory']//*[contains(text(), ' Save')]");
         msgSubName = _webDriverHandler.byXpath("//*[@id='Name-error']");
@@ -106,11 +116,13 @@ public class SubCategories {
         msgSLA = _webDriverHandler.byXpath("//*[@id='NextActionSlaType-error']");
         msgActionDate = _webDriverHandler.byXpath("//*[@id='NextActionInterval-error']");
         msgToClose = _webDriverHandler.byXpath("//*[@id='SubCategoryRequiredToCloseSelect-error']");
-        btnOk = _webDriverHandler.byXpath("//button[contains(text(),'OK')]");
+   //     btnOk = _webDriverHandler.byXpath("//button[contains(text(),'OK')]");
+    btnOk = _webDriverHandler.byXpath("*//button[@type='submit']");
+    btnSuccess=_webDriverHandler.byXpath("*//button[@type='button'][contains(text(),'OK')]");
     }
 
-    public String[] tblSubCategories = new String[]{"Status", "Sub-Category Name", "Category Name", "Customer", "Supplier", "Licensee", "Global", "Update on Create", "Next Step", "Next Action Days", "Actions"};
-    public String[] arrSubCategories = new String[11];
+    public String[] tblSubCategories = new String[]{"Status", "Sub-Category Name", "Category Name",  "Licensee","Customer","Update on Create", "Next Step", "Next Action Days", "Actions"};
+    public String[] arrSubCategories = new String[9];
 
     String strErrorSubName = "The Sub-Category Name: field is required.";
     String strErrorCategory = "The Category Name: field is required.";
@@ -140,7 +152,10 @@ public class SubCategories {
         tbSearch.waitClickable().clear().sendKeys(subCategory);
         waitLoad();
     }
-
+    public void searchName(String name){
+        tbSearch.waitClickable().clear().sendKeys(name);
+        waitLoad();
+    }
     public void subCategoriesTgl (Hashtable<String, Boolean> tglData) {
         if (tglData.get("SR")) {
             if (!tglSupplierRequiredCheck.isSelected()) {
@@ -161,26 +176,6 @@ public class SubCategories {
                 tglUtilityRequired.waitClickable().click();
             }
         }
-
-        if (tglData.get("CA")) {
-            if (!tglCustomerAccessCheck.isSelected()) {
-                tglCustomerAccess.waitClickable().click();
-            }
-        } else {
-            if (tglCustomerAccessCheck.isSelected()) {
-                tglCustomerAccess.waitClickable().click();
-            }
-        }
-        if (tglData.get("SA")) {
-            if (!tglSupplierAccessCheck.isSelected()) {
-                tglSupplierAccess.waitClickable().click();
-            }
-        } else {
-            if (tglSupplierAccessCheck.isSelected()) {
-                tglSupplierAccess.waitClickable().click();
-            }
-        }
-
         if (tglData.get("LA")) {
             if (!tglLicenseeAccessCheck.isSelected()) {
                 tglLicenseeAccess.waitClickable().click();
@@ -191,15 +186,29 @@ public class SubCategories {
             }
         }
 
-        if (tglData.get("GBL")) {
-            if (!tglGlobalCheck.isSelected()) {
-                tglGlobal.waitClickable().click();
+        if (tglData.get("CA")) {
+            if (!tglCustomerAccessCheck.isSelected()) {
+                tglCustomerAccess.waitClickable().click();
             }
         } else {
-            if (tglGlobalCheck.isSelected()) {
-                tglGlobal.waitClickable().click();
+            if (tglCustomerAccessCheck.isSelected()) {
+                tglCustomerAccess.waitClickable().click();
             }
         }
+		/*
+		 * if (tglData.get("SA")) { if (!tglSupplierAccessCheck.isSelected()) {
+		 * tglSupplierAccess.waitClickable().click(); } } else { if
+		 * (tglSupplierAccessCheck.isSelected()) {
+		 * tglSupplierAccess.waitClickable().click(); } }
+		 */
+
+       
+
+		/*
+		 * if (tglData.get("GBL")) { if (!tglGlobalCheck.isSelected()) {
+		 * tglGlobal.waitClickable().click(); } } else { if
+		 * (tglGlobalCheck.isSelected()) { tglGlobal.waitClickable().click(); } }
+		 */
 
         if (tglData.get("DWD")) {
             if (!tglDueWorkDaysCheck.isSelected()) {
@@ -233,17 +242,18 @@ public class SubCategories {
     }
 
     public void subCategoriesForm (Hashtable<String, String> formData) {
-        tbSubCategoryName.waitClickable().clear().sendKeys(formData.get("name"));
+        tbSubCategoryName.waitClickable().clear().sendKeys(formData.get("subCategoryName"));
         ddCategory.selectByText(formData.get("category"));
+        textAreadefaultSummary.sendKeys(formData.get("defaultSummary"));
         ddDefaultAssignment.selectByText(formData.get("defaultAssign"));
         ddNextStep.selectByText(formData.get("nextStep"));
-        tbDueDate.clear().sendKeys(formData.get("dueDate"));
+        tbDueDate.clear().sendKeys(formData.get("defaultDueDate"));
         ddSLA.selectByText(formData.get("actionSLA"));
         tbActionDate.clear().sendKeys(formData.get("actionDate"));
     }
 
-    public void createSubCategories(Hashtable<String, Boolean> tglData,  Hashtable<String, String> formData, String... toClose) {
-        btnNewSubCategory.waitClickable().click();
+    public void createSubCategories(Hashtable<String, Boolean> tglData,  Hashtable<String, String> formData, String... toClose) throws InterruptedException {
+        btnNewSubCategory.waitClickable(15,100).click();
         subCategoriesTgl(tglData);
         subCategoriesForm(formData);
         ddToClose.waitClickable().click();
@@ -252,17 +262,21 @@ public class SubCategories {
                 _webDriverHandler.byXpath("//label[contains(text(), ' " + t + "')]").click();
             }
         }
-        ddToClose.waitClickable().click();
-        btnCreate.waitClickable().click();
-        btnOk.waitClickable().click();
+     //  ddToClose.waitClickable(10,100).click();
+        btnCreate.click();
+        System.out.println("New subcategory is added");
+       btnSuccess.waitClickable(15,100).click();
+       // btnOk.waitClickable(15,100).click();
     }
 
-    public void editSubCategories(String name, Hashtable<String, Boolean> tglData, Hashtable<String, String> formData, String... toClose)  {
+    public void editSubCategories(String name, Hashtable<String, Boolean> tglData, Hashtable<String, String> formData)  
+    //, String... toClose removed from the definition
+    {
         search(name);
         btnEdit.waitClickable().click();
         subCategoriesTgl(tglData);
         subCategoriesForm(formData);
-        ddToClose.waitClickable().click();
+    /*   ddToClose.waitClickable().click();
         for(int i=1; i<5; i++) {
             try {
                 if (_webDriverHandler.byXpath("//div//ul[contains(@class, 'multiselect-container')]//li[" + i + "]//label//input").isSelected()) {
@@ -271,18 +285,21 @@ public class SubCategories {
             } catch (Exception e) {
             }
         }
+        
         for (String t : toClose) {
             if (!(_webDriverHandler.byXpath("//label[contains(text(), ' " + t + "')]//input").isSelected())) {
                 _webDriverHandler.byXpath("//label[contains(text(), ' " + t + "')]").click();
             }
         }
         ddToClose.waitClickable().click();
+        */
         btnSave.waitClickable().click();
         btnOk.waitClickable().click();
         waitLoad();
-        search(formData.get("name"));
+       /* searchName(formData.get("name"));
+        
         if (tglData.get("CA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[4]").getText().equals("Yes"))) {
-        } else if (!tglData.get("CA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[4]").getText().equals("No"))) {
+        	        } else if (!tglData.get("CA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[4]").getText().equals("No"))) {
         } else {
             Assert.fail("Not Working Fine.");
         }
@@ -306,76 +323,9 @@ public class SubCategories {
         } else {
             Assert.fail("Not Working Fine.");
         }
+    */
     }
-
-    public void copySubCategories(String name, Hashtable<String, Boolean> tglData, Hashtable<String, String> formData, String... toClose)  {
-        search(name);
-        btnCopy.waitClickable().click();
-        Assert.assertEquals("Missing Data", ddCategory.getSelectedText());
-        Assert.assertEquals("Project Manager", ddDefaultAssignment.getSelectedText());
-        Assert.assertEquals("Resolve", ddNextStep.getSelectedText());
-        Assert.assertEquals("78", tbDueDate.getAttribute("value"));
-        Assert.assertEquals("Days from Action", ddSLA.getSelectedText());
-        Assert.assertEquals("59", tbActionDate.getAttribute("value"));
-        Assert.assertTrue(tglSupplierRequiredCheck.isSelected());
-        Assert.assertFalse(tglUtilityRequiredCheck.isSelected());
-        Assert.assertTrue(tglCustomerAccessCheck.isSelected());
-        Assert.assertFalse(tglSupplierAccessCheck.isSelected());
-        Assert.assertTrue(tglLicenseeAccessCheck.isSelected());
-        Assert.assertFalse(tglGlobalCheck.isSelected());
-        Assert.assertTrue(tglDueWorkDaysCheck.isSelected());
-        Assert.assertFalse(tglActionWorkDaysCheck.isSelected());
-        Assert.assertTrue(tglStatusCheck.isSelected());
-        Assert.assertFalse(tglCopyFieldsCheck.isSelected());
-        Assert.assertFalse(tglCopyActionsCheck.isSelected());
-        subCategoriesTgl(tglData);
-        subCategoriesForm(formData);
-        ddToClose.waitClickable().click();
-        for(int i=1; i<5; i++) {
-            try {
-                if (_webDriverHandler.byXpath("//div//ul[contains(@class, 'multiselect-container')]//li[" + i + "]//label//input").isSelected()) {
-                    _webDriverHandler.byXpath("//div//ul[contains(@class, 'multiselect-container')]//li[" + i + "]//label").waitClickable().click();
-                }
-            } catch (Exception e) {
-            }
-        }
-        for (String t : toClose) {
-            if (!(_webDriverHandler.byXpath("//label[contains(text(), ' " + t + "')]//input").isSelected())) {
-                _webDriverHandler.byXpath("//label[contains(text(), ' " + t + "')]").click();
-            }
-        }
-        ddToClose.waitClickable().click();
-//        btnCreate.waitClickable().click();
-//        btnOk.waitClickable().click();
-//        waitLoad();
-//        search(formData.get("name"));
-        if (tglData.get("CA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[4]").getText().equals("Yes"))) {
-        } else if (!tglData.get("CA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[4]").getText().equals("No"))) {
-        } else {
-            Assert.fail("Not Working Fine.");
-        }
-        if (tglData.get("SA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[5]").getText().equals("Yes"))) {
-        } else if (!tglData.get("SA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[5]").getText().equals("No"))) {
-        } else {
-            Assert.fail("Not Working Fine.");
-        }
-        if (tglData.get("LA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[6]").getText().equals("Yes"))) {
-        } else if (!tglData.get("LA") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[6]").getText().equals("No"))) {
-        } else {
-            Assert.fail("Not Working Fine.");
-        }
-        if (tglData.get("GBL") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[7]").getText().equals("Yes"))) {
-        } else if (!tglData.get("GBL") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[7]").getText().equals("No"))) {
-        } else {
-            Assert.fail("Not Working Fine.");
-        }
-        if (tglData.get("Sts") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[1]").getText().equals("Active"))) {
-        } else if (!tglData.get("Sts") && (_webDriverHandler.byXpath("//tr[td[contains(text(), '" + formData.get("name") + "')]]//td[1]").getText().equals("Inactive"))) {
-        } else {
-            Assert.fail("Not Working Fine.");
-        }
-    }
-
+  
     public void columnVerification() {
         for (int i = 0; i < arrSubCategories.length; i++) {
             arrSubCategories[i] = _webDriverHandler.byXpath("//table[@id='tblSubCategories']/thead/tr/th[" + (i + 1) + "]").getText();
@@ -389,8 +339,15 @@ public class SubCategories {
     public void errorValidation() {
         btnNewSubCategory.waitClickable().click();
         btnCreate.waitClickable().click();
-        if (msgSubName.getText().equalsIgnoreCase(strErrorSubName) && msgCategory.getText().equals(strErrorCategory) && msgNxtStep.getText().equals(strErrorNxtStep) && msgDueDate.getText().equals(strErrorDueDate)
-        && msgSLA.getText().equals(strErrorSLA) && msgActionDate.getText().equals(strErrorActionDate) && msgToClose.getText().equals(strErrorToClose) && msgDefaultAssignment.getText().equals(strErrorDefaultAssignment)) {
+        if (msgSubName.getText().equalsIgnoreCase(strErrorSubName) 
+        		&& msgCategory.getText().equals(strErrorCategory)
+        		&& msgNxtStep.getText().equals(strErrorNxtStep) 
+        		&& msgDueDate.getText().equals(strErrorDueDate)
+        		&& msgSLA.getText().equals(strErrorSLA)
+        		&& msgActionDate.getText().equals(strErrorActionDate) )
+        	//&& msgToClose.getText().equals(strErrorToClose) 
+    		//&& msgDefaultAssignment.getText().equals(strErrorDefaultAssignment)
+    		{
             System.out.println("Proper error messages are shown.");
         }
         else{
