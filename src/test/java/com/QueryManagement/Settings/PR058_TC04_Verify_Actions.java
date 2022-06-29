add6a158172fbd2cbec96a78ad7e391a40482e78
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-06-29 09:41:49 +0000

    TC04-Verify Actions-Done

--- src/test/java/com/QueryManagement/Settings/PR058_TC04_Verify_Actions.java
+++ src/test/java/com/QueryManagement/Settings/PR058_TC04_Verify_Actions.java
@@ -9,28 +9,29 @@ public class PR058_TC04_Verify_Actions extends AbstractTestNg {
 
     public PR058_TC04_Verify_Actions() throws IOException {
     }
-
+//Pass
     @Test
     public void AccessActions() {
         Actions actions = new Actions(webDriverHandler);
-        actions.accessSettings();
+        //actions.accessSettings();
         actions.accessActions();
         actions.columnVerification();
-        actions.createAction("Automation Action", "Supplier", "Days from Action", "25", true, "Resolve for Customer", true, false);
+        //actions.createAction("Automation Action", "Supplier", "Days from Action", "25", true, "Resolve for Customer", true, false);
+       actions.createAction("Automation Action", "Licensee","Resolve for Customer", true);
     }
-
+//pass
     @Test
     public void EditActions() {
         Actions actions = new Actions(webDriverHandler);
-        actions.accessSettings();
+        //actions.accessSettings();
         actions.accessActions();
-        actions.editAction("Automation Action", "Automation Action Edit", "Customer", "Days before Due Date", "6", false, "None", false, true);
+        actions.editAction("Automation Action", "Automation Action Edited", "Customer","Resolve for Customer", true);
     }
-
+//Pass
     @Test
     public void Validations() {
         Actions actions = new Actions(webDriverHandler);
-        actions.accessSettings();
+        //actions.accessSettings();
         actions.accessActions();
         actions.errorValidation();
     }
