7942db5704b1fbff5e15df0a915bca906afc1cf9
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-07-04 11:02:25 +0000

    QMP-SubCategory-Changes done

--- src/test/java/com/QueryManagement/Settings/PR058_TC06_Verify_SubCategoryFields.java
+++ src/test/java/com/QueryManagement/Settings/PR058_TC06_Verify_SubCategoryFields.java
@@ -34,24 +34,10 @@ public class PR058_TC06_Verify_SubCategoryFields extends AbstractTestNg {
         SubCategoryFields subCategoryFields = new SubCategoryFields(webDriverHandler);
        // subCategoryFields.accessSettings();
         subCategoryFields.accessSubCategoryFields();
-     //   subCategoryFields.createSubCategoryField("Automation SubCategory", "Automation Field Edited", "3", true,"Read/Write", "Read/Write",true);
-       //subCategoryFields.editSubCategoryField("Automation SubCategory", "ML Test", "Automation SubCategory", "Enter Text Area", "3", "Read/Write", "None", "Read Only", false, true, false);
-       subCategoryFields.editSubCategoryField("Automation SubCategory","Automation SubCategory Edited","Automation Field Edited","33",true,"Read/Write","Read/Write",true);
+  subCategoryFields.editSubCategoryField("Automation SubCategory","Automation SubCategory Edited","Automation Field Edited","33",true,"Read/Write","Read/Write",true);
     }
 
-	/*
-	 * @Test public void EditSubCategoryDateFields() { SubCategoryFields
-	 * subCategoryFields = new SubCategoryFields(webDriverHandler);
-	 * subCategoryFields.accessSettings();
-	 * subCategoryFields.accessSubCategoryFields();
-	 * subCategoryFields.columnVerification();
-	 * //subCategoryFields.editSubCategoryField("Automation SubCategory",
-	 * "Enter Date Time", "Automation SubCategory", "Enter Date Time", "5",
-	 * "Read/Write", "Read Only", "None", true, true, true, false, false);
-	 * subCategoryFields.editSubCategoryField("Automation SubCategory",
-	 * "Enter Date", "Automation SubCategory", "Enter Date Time", "5", "Read/Write",
-	 * "Read Only", "None", true, true, true, true, false); }
-	 */
+
 //Pass
     @Test
     public void Validations() {
