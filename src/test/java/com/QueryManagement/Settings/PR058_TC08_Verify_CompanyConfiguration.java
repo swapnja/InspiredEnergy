f15286c848f84134f16039f1e788fa621873ee9a
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-07-05 07:54:51 +0000

    QMP-Settings-CompanyConfiguration-only coloum verification is done and rest of test cases

--- src/test/java/com/QueryManagement/Settings/PR058_TC08_Verify_CompanyConfiguration.java
+++ src/test/java/com/QueryManagement/Settings/PR058_TC08_Verify_CompanyConfiguration.java
@@ -11,19 +11,19 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
 
     public PR058_TC08_Verify_CompanyConfiguration() throws IOException {
     }
-
+//PAss
     @Test
     public void AccessCompanyConfiguration() {
         CompanyConfiguration companyConfiguration = new CompanyConfiguration(webDriverHandler);
-        companyConfiguration.accessSettings();
+       // companyConfiguration.accessSettings();
         companyConfiguration.accessCompanyConfiguration();
         companyConfiguration.columnVerification();
     }
-
+//not done
     @Test
     public void VerifyCompanyPage() {
         CompanyConfiguration companyConfiguration = new CompanyConfiguration(webDriverHandler);
-        companyConfiguration.accessSettings();
+      //  companyConfiguration.accessSettings();
         companyConfiguration.accessCompanyConfiguration();
         companyConfiguration.selectEditEntry("Engie");
         companyConfiguration.verifyCompanyPage();
@@ -31,11 +31,11 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         companyConfiguration.columnVerificationContact();
         companyConfiguration.columnVerificationAssignment();
     }
-
+//not done
     @Test
     public void AddDocuments() {
         CompanyConfiguration companyConfiguration = new CompanyConfiguration(webDriverHandler);
-        companyConfiguration.accessSettings();
+       // companyConfiguration.accessSettings();
         companyConfiguration.accessCompanyConfiguration();
         companyConfiguration.selectEditEntry("Engie");
         String path = Paths.get("PricecastTestData\\SupplierUplifts.docx").toAbsolutePath().toString();
@@ -45,7 +45,7 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         companyConfiguration.downloadDoc("Automation Test");
         companyConfiguration.deleteDoc("Automation Test");
     }
-
+//not done
     @Test
     public void EditDocuments() {
         WebDriverHandler webDriverHandler = new WebDriverHandler(driver);
@@ -59,12 +59,12 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         companyConfiguration.editDocument("For Edit", "After Edit", "LOA", accept, send);
         companyConfiguration.editDocument("After Edit", "For Edit", "KT Session", accept, send);
     }
-
+//not done
     @Test
     public void AddContacts() {
         WebDriverHandler webDriverHandler = new WebDriverHandler(driver);
         CompanyConfiguration companyConfiguration = new CompanyConfiguration(webDriverHandler);
-        companyConfiguration.accessSettings();
+     //   companyConfiguration.accessSettings();
         companyConfiguration.accessCompanyConfiguration();
         companyConfiguration.selectEditEntry("Engie");
         String[] attach = {"Faulty Meter",  "KT New Subcat", "Performance Major"};
@@ -72,7 +72,7 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         companyConfiguration.addContact("rahul.dash@cloudenergysoftware.com", "Me","1234567890", "Administrator", attach, send);
         companyConfiguration.deleteContact("rahul.dash@cloudenergysoftware.com");
     }
-
+//not done
     @Test
     public void EditContacts() {
         WebDriverHandler webDriverHandler = new WebDriverHandler(driver);
@@ -84,7 +84,7 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         String[] send = {"Account Query",  "Meter Reading", "MOP Visit"};
         companyConfiguration.editContact("fake.id@cloudenergysoftware.com", attach, send);
     }
-
+//not done
     @Test
     public void AddEditAssignment() {
         WebDriverHandler webDriverHandler = new WebDriverHandler(driver);
@@ -98,7 +98,7 @@ public class PR058_TC08_Verify_CompanyConfiguration extends AbstractTestNg {
         companyConfiguration.editAssignment("IP DEV TP2", "Account Contact", "Test Manager", subCat);
         companyConfiguration.deleteAssignment("Test Manager");
     }
-
+//not done
     @Test
     public void ErrorValidations() {
         WebDriverHandler webDriverHandler = new WebDriverHandler(driver);
