708ef347876e6c2f2f52500a31007dc1954ee2cc
swapnja <swapnja.gudle@cloudeenrgysoftware.com>
2022-07-13 10:31:36 +0000

    QMP-Category-Create Task

--- src/test/java/com/QueryManagement/Home/PR058_TC01_Verify_Create_New.java
+++ src/test/java/com/QueryManagement/Home/PR058_TC01_Verify_Create_New.java
@@ -9,22 +9,23 @@ public class PR058_TC01_Verify_Create_New extends AbstractTestNg {
 
     public PR058_TC01_Verify_Create_New() throws IOException {
     }
-
+//Pass
     @Test
     public void VerifyCreateNewTask() {
         CreateNewTask createNewTask = new CreateNewTask(webDriverHandler);
         createNewTask.accessCreateNew();
         createNewTask.verifyCreateNewModal();
     }
-
+//pass :) // there is "Automation Field:" field coming with the Automation Category and Automation SubCategory1  
     @Test
-    public void CreateNewTask() {
+    public void CreateNewTask() {     
         CreateNewTask createNewTask = new CreateNewTask(webDriverHandler);
         createNewTask.accessCreateNew();
-        createNewTask.createNewTask("Customer", "Billing", "Incorrect Rates", "Opus", "The summary to be put in.", false,
-                true, "Electricity");
+        createNewTask.createNewTask("Chirayu Cust", "Automation Category", "Automation SubCategory 6",
+        		"CNG","Electricity", "Automation test Summary", true,
+                true,"Inspired Plc","28/07/2022","28/07/2022","22");
     }
-
+  //form[@id='myForm']/input[@name='version']/@value
 //    @Test
 //    public void EditValidations() {
 //        CreateNewTask createNewTask = new CreateNewTask(webDriverHandler);
